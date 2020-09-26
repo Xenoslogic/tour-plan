8377a0dcd39e290150464c0db3a92aace88545c6
Xenoslogic <sdv666999@gmail.com>
2020-09-26 13:08:58 +0300

    Create: other-packages section and responsive

--- js/main.js
+++ js/main.js
@@ -34,48 +34,7 @@ $(document).ready(function () {
 		imageSrc: "img/newsletter-bg.jpg",
 	});
 
-	// мобильное меню
-	const menuButton = document.querySelector(".menu-button");
-	menuButton.addEventListener("click", () => {
-		document.querySelector(".navbar-menu").classList.toggle("navbar-menu_visible");
 
-		const srcImage = document.querySelector(".menu-button__image").src;
-
-		if (srcImage === window.location.href.toString() + "img/three-bars.svg") {
-			changeImage("img/x.svg");
-		} else if (srcImage === window.location.href.toString() + "img/x.svg") {
-			changeImage("img/three-bars.svg");
-		}
-	});
-
-	function changeImage(pathImg) {
-		document.querySelector(".menu-button__image").src = pathImg;
-	}
-
-	// Модальные окна
-	const closeModalButton = $(".modal__close");
-	closeModalButton.on("click", closeModal);
-	$(document).keyup((e) => {
-		if (e.key === "Escape") {
-			closeModal(e);
-		}
-	});
-
-	function closeModal(event) {
-		event.preventDefault();
-		$(".modal__overlay").removeClass("modal__overlay_visible");
-		$(".modal__dialog").removeClass("modal__dialog_visible");
-	}
-
-	let modalButton = $("[data-toggle=modal]");
-	modalButton.on("click", openModal);
-
-	function openModal() {
-		let targetModal = $(this).attr("data-href");
-
-		$(targetModal).find(".modal__overlay").addClass("modal__overlay_visible");
-		$(targetModal).find(".modal__dialog").addClass("modal__dialog_visible");
-	}
 
 	// валидация
 	$("[name=phone]").mask("+7(000) 000-00-00");
@@ -140,13 +99,5 @@ $(document).ready(function () {
 		});
 	});
 
-	// карты
-	$(".hotel-about").on("mousemove", function () {
-		$(".map__frame").attr(
-			"src",
-			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35768.629794727676!2d126.9271982856096!3d37.589681673921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c986a45705bb5%3A0x224be1d558aa33!2sSwiss%20Grand%20Hotel!5e0!3m2!1sru!2sru!4v1595847140951!5m2!1sru!2sru"
-		);
-		$(".hotel-about").off("mousemove");
-		$(".map__image").css("display", "none");
-	});
+	
 });
