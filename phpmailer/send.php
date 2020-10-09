21a1f1abd7c81484d21f6e3668b271dd3b05561f
Xenoslogic <sdv666999@gmail.com>
2020-10-09 16:24:33 +0300

    Fix:

--- phpmailer/send.php
+++ phpmailer/send.php
@@ -26,21 +26,21 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
     $mail->isSMTP();   
     $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
+    $mail->SMTPAuth = true;
     $mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
     $mail->Host       = 'smtp.rambler.ru'; // SMTP сервера вашей почты
     $mail->Username   = 'serdtsevdv@rambler.ru'; // Логин на почте
-    $mail->Password   = ''; // Пароль на почте
+    $mail->Password   = 'UJhljcnmGHfqlf1364'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('serdtsevdv@rambler.ru', 'Dmitriy'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
     $mail->addAddress('serdtsevdv@rambler.ru');  
-    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+    // $mail->addAddress('dmitry.hopkins@yandex.ru'); // Ещё один, если нужен
 
     
 // Отправка сообщения
